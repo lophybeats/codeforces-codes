6a0dba79d3b56ca6825db2f5376d3992ffadf471
return_z <54408718+lophybeats@users.noreply.github.com>
2020-02-04 13:39:03 +0530

    Create cf#612(B).cpp

---
+++ cf#612(B).cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    double n;
+    cin >> n;
+    double sum = 0;
+    for (double i=1;i<=n;i++){
+        sum += (1/i);
+    }
+    cout << sum << endl;
+}
